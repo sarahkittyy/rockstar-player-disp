b03fdded05fa1d5c7cb770260aa00556af1a403c
sarahkittyy <sarahohlin1@gmail.com>
2019-07-07 18:27:13 -0400

    more config settings

--- src/Settings.ts
+++ src/Settings.ts
@@ -4,4 +4,5 @@ export default interface Settings
 	path: string; // The base path to all SCP servers.
 	nickname: string; // The nickname to update with the player count.
 	//^ Updated by replacing {} with the current player count.
+	guildid: string; // The guild's ID.
 };
